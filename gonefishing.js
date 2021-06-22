aabd3f5a59fdd81b36de44b211ce81ed74deb6e2
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-06-22 12:51:51 -0400

    Begin fishing project

---
+++ gonefishing.js
@@ -0,0 +1,4 @@
+//This is my Gone Fishing game, and my first comments here. I plan to begin by creating the functions,
+//which will create the randomly generated fish. This 'part' of the project can exist entirely by
+//itself, so I think it's a good place to just get something going! I will now go make my first commit.
+
