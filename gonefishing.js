bedcf70a6e7e61a4a3568654aca040e32e22227f
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-06-23 12:36:21 -0400

    Clean up functions & add strart screen

--- gonefishing.js
+++ gonefishing.js
@@ -4,6 +4,10 @@
 
 console.log ('And so it begins.');
 
+//Our old friend prompt-sync
+const prompt = require('prompt-sync')();
+
+
 //list of colors
 const fishColors = ["red", "orange", "blue", "green", "brown", "yellow", "orange with blue stripes", "transluscent", "brown with red stripes", 
 "red with yellow stripes", "black", "green with brown spots", "brown with white spots", "white with red spots"];
@@ -22,26 +26,16 @@ const fishType = ["catfish", "trout", "salmon", "chub", "pike", "bass", "sturgeo
 //Starting values for important objects & arrays
 let bucket = [];
 let onRod = [];
+let clock = 0;
 
 
-//I have some questios about accessing a function without the () operator. As I understand it,
-//without any input there, this function will return the OBJECT I am creating - the new fish.
-//Currently I am pushing this fish into an array outside of the function, but performing
-//this action INSIDE of my function.
-//
-//If I removed the line 'onRod.push(newFish);', what's another way to get this object
-//where I want it to go? I may just be confused on the syntax here.
-//
-//TODO: need value & weight
+
+//-------------FUNCTIONS---------------//
 function createFish () {
 
     //First we clear the rod, I might move this line to somewhere else in the program.
     onRod = [];
     const newFish = {
-
-        //I don't need to use a function here, but thought it would be fun to call a function
-        //inside of another function. I could just copy over the random code into each line,
-        //but change the variables so it matches the array I want to pull from.
         color : random_item(fishColors),
         desc : random_item(fishDesc),
         type : random_item(fishType),
@@ -50,12 +44,7 @@ function createFish () {
         value : randomCash(1),
         weight : randomWeight(1),
     }
-    //This is a temporary storage place for the fish. I think from here the player will "keep",
-    //meaning splice the onRod object into the bucket array; or, "throw back" where I will
-    //just clear the onRod array and delete the newly created fish.
-    //
-    //Two choices: add in a ternary statement with a random roll, to decide if the line breaks,
-    //or different functions/systems to pick the result of the players "cast" of the fishing line.
+    //This is a temporary storage place for the fish, while player makes a decision.
     onRod.push(newFish);
     return
 }
@@ -63,24 +52,14 @@ function createFish () {
 // createFish();
 // console.log('This is what we caught...', onRod);
 
-
-//This function will pull a random element from a chosen array. I have an idea to use this
-//during the 'fishing' part of my game. If I wanted there to be a 1/10 chance of 'breaking
-//the line' on the fishing pole; I could make an array with 10 values [create fish, create fish, 
-//create fish, ... break line, create fish], and have this function pull randomly. The player 
-//would then either get a fish 9/10 times, or break the line the other 1/10 of the time. Many 
-//approaches to this already I can see.
-//
 //This code I copied from a website, but it's so simple I did not think it would be an
 //issue if I used it. I don't think I have ever used an '_' in a function name.
 function random_item(items) { 
     return items[Math.floor(Math.random()*items.length)];
 }
 
-
-//functions for weight and value
-//this is more difficult because the values here, will determine how 'difficult' our game
-//is. I think I want to give the player 3 chances to catch a fish each hour, but each
+//This is more difficult because the values here, will determine how 'difficult' our game
+//is. I want to give the player 3 chances to catch a fish each hour, but each
 //chance will not always produce a fish. The line could break, for example, or they
 //could fish up a piece of junk.
 function randomWeight (a) {
@@ -126,9 +105,9 @@ function randomCash (a) {
 
 
 
-//I would like to potentially add in a rarity system. I could create a data set for Bass,
+//I would like to potentially add in a rarity system. I could create a data range for Bass,
 //where they are always 1-3lbs in weight and $0.50 - $2 in value. Bass would have a PPP 
-//(price-per-pound) raneg of 0.166 - 2. Any Bass with a PPP > 1.541 would be in the top
+//(price-per-pound) range of 0.166 - 2. Any Bass with a PPP > 1.541 would be in the top
 //25% of possible Bass to catch. This would trigger an additional property to be assigned
 //to the fish, to help the player understand what they have.
 //
@@ -154,16 +133,13 @@ function lookBucket () {
         sumCash += Number(bucket[i].value);
     }
     
-    //Trivial case
+    //Trivial case and output to player
     return bucket.length === 0 ? console.log('Your bucket is empty, go catch some fish!') : 
     console.log('\n', 'You currently have', sumFish.toString(), 'fish.', '\n',
                 'Your bucket weighs', sumWeight.toFixed(2),'lbs.', '\n',
                 'This haul is worth $', sumCash.toFixed(2), 'back in town.'
                );
 }
-
-lookBucket();
-
 
 //Function to move the clock, with a variable containing the # of minutes to advance.
 //Let's do the timewarp again
@@ -172,7 +148,7 @@ clock += warpSpeed;
     return 
 }
 
-timeWarp(100);
+// timeWarp(100);
 
 //This function will convert the value in clock (minutes) to the 24hr clock standard.
 //It is easier to do this than convert afternoon times, 13 -> 1, 14 -> 2 ...
@@ -184,7 +160,33 @@ let minutes = clock % 60;
     //I used padStart here to append the hours for the 24hr clock. Numbers are output to
     //terminal in yellow which stands out visually; I converted these values to strings
     //for consistency with the rest of my program.
-    return console.log('\n', 'It is', String(hours).padStart(2,'0'), ':', minutes.toString())
+    return console.log('\n', 'It is', String(hours).padStart(2,'0'), ':', String(minutes).padStart(2,'0'));
 }
 
-checkWatch ();
+//
+//-------END OF FUNCTIONS---------//
+//
+
+//Opening splash screen for player.
+console.clear ();
+console.log('Welcome to',
+             '\n',  '\n', '\n', '\n', 
+            '   Reel Out: Big Bass American Fishing Challenge 2021 - the Terminal Edition',
+            '\n', '\n', 
+            '                       "Where any fin is possible"', 
+            '\n', '\n', '\n',);
+console.log('Press ENTER to START!');
+prompt ('>');
+
+//We take in a custom value for the player's name.
+console.log('What is your name?')
+const name = prompt ('>');
+console.clear ();
+
+//Here we set the time to 0600.
+timeWarp(360);
+
+console.log('~~~~~~~ REEL OUT! ~~~~~~~',
+            '\n',
+            '\n',);
+checkWatch();
